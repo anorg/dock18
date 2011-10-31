89539a36830a41e1bf431e589bb64d49c27df3a9
ohrstrom <jonas@anorg.net>
2011-10-31 23:33:00 +0100

    still reworking

--- website/ajax/ajax.py
+++ website/ajax/ajax.py
@@ -48,7 +48,7 @@ def loopcount(request):
                 
                 total = element[1].text
                 current = element[0].text
-                bw = str(float(element[5].text) / 1000000) + 'MBps'
+                bw = str(float(element[5].text) / 1000) + ' KBps'
             
         except Exception, e:
             total = 'error'
