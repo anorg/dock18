2609052b5e52f52c661150c5d974c792f767a6ec
Hugo Hackenstein <sh@sighup.ch>
2011-11-06 14:28:15 +0100

    remove test comments

--- website/feedback/utils.py
+++ website/feedback/utils.py
@@ -4,15 +4,6 @@ from django.template import loader
 from django.core.exceptions import ImproperlyConfigured
 from django.conf import settings
 
-
-# some comment
-
-# we add another one
-
-# even more test:
-
-# fufufu
-
 def email_backend(recipient_list, message, subject='Feedback'):
     subject = settings.EMAIL_SUBJECT_PREFIX + subject
     if type(recipient_list) is not list:
