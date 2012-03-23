a1a62485048fe551d867422732f5c3f000f18f92
ohrstrom <jonas@anorg.net>
2012-03-23 15:27:23 +0100

    fixed install & config scripts

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,178 @@
 #!/usr/bin/env python
 from __future__ import with_statement
-from fabric.api import local, settings, abort, run, cd
+from fabric.api import local, settings, abort, run, cd, env
 from fabric.contrib.console import confirm
+import os
 
+# glogal env
+env.warn_only = True
+env.supervisor = '/etc/supervisor/conf.d'
+env.nginx = '/etc/nginx/sites-enabled'
 
-def activate():
-    local('echo "source ~/srv/dock18_ch/bin/activate"')
-
-    
 def clean():
     local("find . -name '*.DS_Store' -type f -delete")
     local("find . -name '*.pyc' -type f -delete")
 
+def tx_make():
+    local('cd website && ./manage.py makemessages --all \
+           --no-obsolete \
+           --domain=django \
+           --ignore=ajax/* \
+           --ignore=analytics/* \
+           --ignore=cms* \
+           --ignore=dev/* \
+           --ignore=fixtures/* \
+           --ignore=lib/* \
+           --ignore=locale/* \
+           --ignore=media/* \
+           --ignore=multilingual/* \
+           --ignore=profiles/* \
+           --ignore=social_auth/* \
+           --ignore=taggit/* \
+           --ignore=socialregistration/* \
+           --ignore=filer/*')
     
-def deploy():
-    script_dir = '/var/www/ende.dock18.ch/scripts'
-    with settings(host_string='root@node05.daj.anorg.net'):
-        with cd(script_dir):
-            run('./install_base.sh')
+def tx_push():
+    local('tx push -t -s') 
 
+def tx_pull():
+    """
+    push translations
+    """
+    local('tx pull')
+    local('cd website && ./manage.py compilemessages') 
+    
+
+
+
+
+"""
+ad definition for your domains, eg:
+ende.dock18.ch
+"""
+def ende_dock18_ch():
+    env.site_id = 'ende.dock18.ch'
+    env.hosts = ['176.34.167.24']
+    env.git_url = 'git@lab.anorg.net:asite.git'
+    env.git_branch = 'master'
+    env.hosts = ['176.34.167.24']
+    env.path = '/var/www/ende.dock18.ch'
+    env.storage = '/var/www_data/ende.dock18.ch'
+    env.user = 'root'
+    
+
+def deploy():
+    """
+    """
+    with cd(env.path):  
+        
+        """
+        create directory to save the local_config
+        """
+        try:
+            run('mkdir config')
+        except Exception, e:
+            print e
+            pass
+        
+        try:
+            run('cp src/website/local_settings.py config/')  
+        except Exception, e:
+            print e
+        
+        """
+        recreate src directory
+        """    
+        try:
+            run('rm -Rf src')
+        except Exception, e:
+            print e
+        
+        run('mkdir src')
+
+        
+    with cd(env.path + '/src'):
+        
+        """
+        aquire code from repository
+        """
+        run('git init')
+        run('git remote add -t %s -f origin %s' % (env.git_branch, env.git_url))
+        run('git checkout %s' % (env.git_branch))
+        
+    with cd(env.path): 
+
+        """
+        copy back the local_settings
+        """
+        try:
+            run('cp config/local_settings.py src/website/')
+        except Exception, e:
+            print e
+            
+        
+        """
+        virtualenv and requirements
+        """
+        try:
+            run('virtualenv --no-site-packages /srv/%s' % env.site_id)
+        except Exception, e:
+            print e
+
+        run('pip -E /srv/%s install -r %s' % (env.site_id, 'src/website/requirements/requirements.txt'))
+        
+            
+        """
+        linking storage directories
+        """
+        try:
+            run('ln -s %s/media %s/src/website/media' % (env.storage, env.path))
+            run('ln -s %s/smedia %s/src/website/smedia' % (env.storage, env.path))
+        except Exception, e:
+            print e
+            
+            
+        """
+        linking config files
+        """
+        try:
+            run('rm %s/%s.conf' % (env.supervisor, env.site_id))
+            run('ln -s %s/src/conf/%s.supervised.conf %s/%s.conf' % (env.path, env.site_id, env.supervisor, env.site_id))
+        except Exception, e:
+            print e
+            
+        try:
+            run('rm %s/%s' % (env.nginx, env.site_id))
+            run('ln -s %s/src/conf/%s.nginx.conf %s/%s' % (env.path, env.site_id, env.nginx, env.site_id))
+        except Exception, e:
+            print e
+            
+        """
+        run migrations
+        """
+        try:
+            run('/srv/%s/bin/python /%s/src/website/manage.py migrate' % (env.site_id, env.path))
+        except Exception, e:
+            print e
+            
+        """
+        staticfiles & compress
+        """
+        try:
+            run('/srv/%s/bin/python /%s/src/website/manage.py collectstatic --noinput' % (env.site_id, env.path))
+            run('/srv/%s/bin/python /%s/src/website/manage.py compress -f' % (env.site_id, env.path))
+        except Exception, e:
+            print e
+            
+        """
+        (re)start gunicorn worker
+        """
+        try:
+            run('supervisorctl restart %s' % env.site_id)
+            run('supervisorctl status')
+        except Exception, e:
+            print e
+        
+        
+        
+        
